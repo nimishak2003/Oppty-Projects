6c25df4e3dc94a07ffec18ec4d0d1c3a13a01b90
nimishak2003 <nimishakadirappagari03@gmail.com>
2025-12-22 12:05:19 +0530

    Exam

---
+++ DAY-8/Task.js
@@ -0,0 +1,17 @@
+// User details
+const user = {
+    Name: "Nimisha",
+    Age: 21,
+    Gender: "Female",
+    Mobile No: "9502361284"
+    Gmail: "nimishakadirappagari03@gmail.com"
+
+
+
+};
+
+// Display data in HTML
+document.getElementById("name").innerText = user.name;
+document.getElementById("age").innerText = user.age;
+document.getElementById("gender").innerText = user.gender;
+document.getElementById("mobile").innerText = user.mobile;
