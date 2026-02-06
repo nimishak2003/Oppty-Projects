780fb176c5c7d00d622bcb3d00ca0adfbab174d5
nimishak2003 <nimishakadirappagari03@gmail.com>
2026-02-07 02:49:16 +0530

    update

--- DAY-44/Palindrome Number/06-02-26Palindrome.js
+++ DAY-44/Palindrome Number/06-02-26Palindrome.js
@@ -1,21 +1,21 @@
 function checkPalindrome() {
-        let num = document.getElementById("num").value;
-        let result = document.getElementById("result");
+  let num = document.getElementById("num").value;
+  let result = document.getElementById("result");
 
-        if (num === "") {
-          result.style.color = "red";
-          result.innerHTML = "❌ Please enter a number";
-          return;
-        }
+  if (num === "") {
+    result.style.color = "red";
+    result.innerHTML = "❌ Please enter a number";
+    return;
+  }
 
-        let original = num;
-        let reversed = num.split("").reverse().join("");
+  let original = num;
+  let reversed = num.split("").reverse().join("");
 
-        if (original === reversed) {
-          result.style.color = "green";
-          result.innerHTML = `✅ ${original} is a PALINDROME number`;
-        } else {
-          result.style.color = "blue";
-          result.innerHTML = `🔵 ${original} is NOT a palindrome number`;
-        }
-      }
+  if (original === reversed) {
+    result.style.color = "green";
+    result.innerHTML = `✅ ${original} is a PALINDROME number`;
+  } else {
+    result.style.color = "blue";
+    result.innerHTML = `🔵 ${original} is NOT a palindrome number`;
+  }
+}
